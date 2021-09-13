2ce3537950a1ee613942a47f9e907ed4a24d472d
Vertti Aito <vertti.aito@sartorius.com>
2021-09-13 12:10:57 +0300

    Fix use of boost::bind

--- roscpp_tutorials/listener_with_userdata/listener_with_userdata.cpp
+++ roscpp_tutorials/listener_with_userdata/listener_with_userdata.cpp
@@ -27,6 +27,7 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <boost/bind/bind.hpp>
 
 /**
  * This tutorial demonstrates a simple use of Boost.Bind to pass arbitrary data into a subscription
@@ -48,9 +49,9 @@ public:
 
   void init()
   {
-    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, _1, "User 1")));
-    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, _1, "User 2")));
-    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, _1, "User 3")));
+    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, boost::placeholders::_1, "User 1")));
+    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, boost::placeholders::_1, "User 2")));
+    subs_.push_back(node_handle_.subscribe<std_msgs::String>("chatter", 1000, boost::bind(&Listener::chatterCallback, this, boost::placeholders::_1, "User 3")));
   }
 
   void chatterCallback(const std_msgs::String::ConstPtr& msg, std::string user_string)
